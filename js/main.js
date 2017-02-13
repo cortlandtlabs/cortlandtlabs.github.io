9fde0671f868fc245f325ff1d4827496575cb0b3
Aaron <aattsai@gmail.com>
2017-02-12 19:41:18 -0500

    Add basic animation

--- js/main.js
+++ js/main.js
@@ -1,24 +1,7 @@
 $(document).ready(function() {
-  $('.portfolio').click(function(event) {
-    $.smoothScroll({
-      scrollTarget: '#portfolio',
-      speed: 1800,
-    });
-  });
-  $('.values').click(function(event) {
-    $.smoothScroll({
-      scrollTarget: '#values',
-      speed: 1800,
-    });
-  });
-  $('.team').click(function(event) {
-    $.smoothScroll({
-      scrollTarget: '#team',
-      speed: 1800,
-    });
-  });
-  $("#typing").typed({
-      strings: ["architect,^350 code,^350 deploy.", ],
-      typeSpeed: 60,
-  });
+  window.sr = ScrollReveal();
+  sr.reveal('.card', { duration: 1800 });
+});
+$(window).on('load', function() {
+   $("#cover").hide();
 });
