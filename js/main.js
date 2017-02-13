3e911dcb2b9d5b651ec0f6e69850d4b4a87adaf1
Aaron <aattsai@gmail.com>
2017-02-13 12:17:21 -0500

    Link start building to contact form for now

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   window.sr = ScrollReveal({ reset: true });
   sr.reveal('.card', { duration: 1800 });
+  $('.build').click(function(event) {
+    $.smoothScroll({
+      scrollTarget: '#contact',
+      speed: 1800,
+    });
+  });
 });
 $(window).on('load', function() {
    $("#cover").hide();
